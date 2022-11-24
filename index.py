d9e9dd897446ee509df10321debd79ea78327aeb
TeraBytes <lwsusername@gmail.com>
2022-11-24 23:24:42 +0800

    First working bot!!!

--- index.py
+++ index.py
@@ -1,24 +1,24 @@
 import discord
 import json
-import time
+import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-devconfig = json.load(open("DO NOT PUSH THIS FILE.json"))
+devconfig = json.load(open('DO NOT PUSH THIS FILE.json'))
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {0.user}'.format(client), datetime.datetime.now(), "UTC")
+    print('We have logged in as {0.user}'.format(client), datetime.datetime.now(), "UTC")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$test'):
+        await message.channel.send('It works!')
 
-client.run(DEVCONFIG['TOKEN'])
+client.run(devconfig['TOKEN'])
